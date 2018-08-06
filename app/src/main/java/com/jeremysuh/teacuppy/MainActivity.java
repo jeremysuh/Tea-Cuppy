2b7faeb5d07a93dc50a3d550dca3ab80f1fdd7da
jeunghwansuh <jeunghwansuh@gmail.com>
2018-08-06 23:18:25 +0700

    Added random comment

--- app/src/main/java/com/jeremysuh/teacuppy/MainActivity.java
+++ app/src/main/java/com/jeremysuh/teacuppy/MainActivity.java
@@ -38,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //random comment
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
