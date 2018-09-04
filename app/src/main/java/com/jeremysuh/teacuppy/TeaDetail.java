211681dea2e9949461fda49920d9545b69c5d116
jeunghwansuh <jeunghwansuh@gmail.com>
2018-09-03 21:28:37 -0400

    Modified tea detail layout

--- app/src/main/java/com/jeremysuh/teacuppy/TeaDetail.java
+++ app/src/main/java/com/jeremysuh/teacuppy/TeaDetail.java
@@ -1,11 +1,11 @@
 package com.jeremysuh.teacuppy;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.MenuItem;
 
 public class TeaDetail extends AppCompatActivity {
 
@@ -16,14 +16,10 @@ public class TeaDetail extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+
+
     }
+
 
 }
